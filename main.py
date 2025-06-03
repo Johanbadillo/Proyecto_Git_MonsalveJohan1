0173c75b48f7ca31d0b0974586dd2502170bdf83
johan <johanbadillomj@gmail.com>
2025-06-03 14:56:29 -0500

    feat:sparkles: second commit prueba conflit

--- main.py
+++ main.py
@@ -2,4 +2,16 @@
 #   Proyecto git Prueba
 # #########################
 
-
+salida=True
+while(salida):
+    print("Bienvenido a nuestro programa de empanadas Doña Pepa\
+        \n\
+        \n=====================================\
+        \n           Menu Principal             \
+        \n======================================\
+        \nGestion de Datos:\
+        \n1. Agregar dato nuevo\
+        \n2. Editar dato\
+        \n3. Eliminar dato\
+        \n4. Salir")
+    Opcion=int(input("Ingrese una opcion numerica\n"))
